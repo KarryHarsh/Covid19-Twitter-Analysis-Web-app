0c218de7f9f9efc21314401d37a7e308d609eb60
Harsh, Karry <karry.harsh@capgemini.com>
2020-06-24 04:26:03 +0530

    Airline streamlit application

--- app.py
+++ app.py
@@ -1,42 +1,75 @@
 import streamlit as st
 import pandas as pd
-import pickle
+import plotly.express as px
+from wordcloud import WordCloud, STOPWORDS
+import matplotlib.pyplot as plt
 
-# title
-import TweetNormalizer
+st.title("Sentiment analysis of tweets of US Airlines")
+st.sidebar.title("Sentiment analysis of tweets of US Airlines 🐦")
 
-st.title("Covid twitter analyzer")
+st.markdown("This application is a Streamlit dashboard to analyze the sentiment of Tweets 🐦")
+st.sidebar.markdown("This application is a Streamlit dashboard to analyze the sentiment of Tweets 🐦")
 
-# Header
-st.header("header")
-# sub header
-st.subheader("subheader")
+DATA_URL = "dataset/Tweets.csv"
 
-# text
-st.text("text")
+@st.cache(persist=True)
+def load_data(URL):
+    df = pd.read_csv(URL)
+    df["tweet_created"] = pd.to_datetime(df["tweet_created"])
+    return df
 
-# Markdown
-st.title("Tweet Analyzer 🔥")
+data = load_data(DATA_URL)
 
-def load_data():
-    return pd.read_excel("dataset/predictv1.1.xlsx")
+st.sidebar.subheader("Show Random Tweet")
+random_tweet = st.sidebar.radio("Sentiment",("positive","negative","neutral"))
+st.sidebar.markdown(data.query("airline_sentiment == @random_tweet")[["text"]].sample(n=1).iat[0,0])
 
-def data_precosess():
-    df = load_data()
-    df["clean_tweets"] = df["tweet"].apply(lambda x: TweetNormalizer.tweet_preprocessing(x))
-    df.dropna(subset=['clean_tweets'], inplace=True)
-    pred_texts = df['clean_tweets']
-    return pred_texts
+st.sidebar.markdown("### Number of tweets by sentiment")
+select = st.sidebar.selectbox("Visualization type", ["Histogram","Pie chart"], key="1")
+sentiment_count = data["airline_sentiment"].value_counts()
+sentiment_count = pd.DataFrame({"Sentiment": sentiment_count.index, "Tweets":sentiment_count.values})
 
-model = st.sidebar.selectbox("Select Classifier",("SVM","LSTM","Bertweet"))
+if not st.sidebar.checkbox("Hide", True):
+    st.markdown("### Number of tweets by sentiment")
+    if select == "Histogram":
+        fig = px.bar(sentiment_count, x="Sentiment", y="Tweets", height=500)
+        st.plotly_chart(fig)
 
-if model == "SVM":
-    model = pickle.load(open("Models/SVM_model.pkl", 'rb'))
-    text = data_precosess()
-    prediction = model.predict(text)
-    st.write(len(prediction))
+    elif select == "Pie chart":
+         fig = px.pie(sentiment_count, values="Tweets", names="Sentiment", height=500)
+         st.plotly_chart(fig)
 
+st.sidebar.subheader("When and where are users tweeting from?")
+hour = st.sidebar.slider("Hour of day", 0,23)
+modified_data = data[data["tweet_created"].dt.hour == hour]
+if not st.sidebar.checkbox("Close", True, key="1"):
+    st.markdown("### Tweets locations based on the time of day")
+    st.markdown("%i tweets between %i:00 and %i:00" % (len(modified_data), hour, (hour+1)%24))
+    #st.map(modified_data)
+    if st.sidebar.checkbox("Show raw data", False):
+        st.write(modified_data)
 
+st.sidebar.subheader("Breakdown airline tweets by sentiment")
+choice = st.sidebar.multiselect("Pick airlines", data.airline.unique(), key= "0")
 
+if len(choice)>0:
+    choice_data = data[data.airline.isin(choice)]
+    fig_choice = px.histogram(choice_data, x="airline", y="airline_sentiment",
+                              histfunc="count", color="airline_sentiment",
+                              facet_col="airline_sentiment",labels={"airline_sentiment": "tweets"},
+                              height=600,width=800)
+    st.plotly_chart(fig_choice)
 
-
+st.sidebar.header("Word Cloud")
+word_sentiment = st.sidebar.radio("Diasplay word cloud for what sentiment?",
+                                  data.airline_sentiment.unique())
+if not st.sidebar.checkbox("Close", True,key = "3"):
+    st.subheader("word cloud for %s sentiment" % (word_sentiment))
+    df1= data[data["airline_sentiment"]==word_sentiment]
+    words = ' '.join(df1["text"])
+    processed_words = " ".join([word for word in words.split() if "http" not in word and not word.startswith("@") and word != "RT"])
+    wordcloud = WordCloud(stopwords=STOPWORDS, background_color="white", height=640, width=800).generate(processed_words)
+    plt.imshow(wordcloud)
+    plt.xticks([])
+    plt.yticks([])
+    st.pyplot()
